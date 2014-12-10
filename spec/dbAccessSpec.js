51e92b27cd4073015828cd88fda07a7c6a339c93
YoshiyukiKato <yo.shuuuuki@gmail.com>
2014-12-10 18:36:04 +0900

    separate module and test

--- spec/dbAccessSpec.js
+++ spec/dbAccessSpec.js
@@ -3,20 +3,20 @@
 
 //++precondition++
 //this require a table "specdb" for running test:
-////CREATE TABLE specdb(id int, name varchar(10));
-////INSERT INTO specdb(1,test1);
-////INSERT INTO specdb(2,test2);
-////INSERT INTO specdb(3,test3);
+////CREATE TABLE specdb(uid varchar(10), passwd varchar(10));
+////INSERT INTO specdb VALUES('test1', 'passwd1');
+////INSERT INTO specdb VALUES('test2', 'passwd2');
+////INSERT INTO specdb VALUES('test3', 'passwd3');
 
-var dbCli = require("../lib/appServer/dbAccess.js").initDbAccess(process.env.dburl);
+var dbCli = require("../lib/appServer/dbClient/dbAccess.js").initDbAccess(process.env.dburl);
 
 describe("SQL Query Test",function(){
     it("should return array of object data",function(done){
-        dbCli.query("SELECT * FROM specdb", function(result,err){
+        dbCli.query("SELECT * FROM specdb;", function(result,err){
             expect(err).toBe(undefined);
             console.log(result);
+            dbCli.close();
             done();
         });
     });
 });
-
