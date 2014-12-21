4ae79b675ea11f221c90d4d98177fea38eb50f2b
YoshiyukiKato <yo.shuuuuki@gmail.com>
2014-12-21 23:58:01 +0900

    add account to users

---
+++ db/migrations/20141221205038_add_account_to_user.js
@@ -0,0 +1,13 @@
+'use strict';
+
+exports.up = function(knex, Promise) {
+    return knex.schema.table('users', function (table) {
+        table.integer('account').defaultTo(0);
+    })
+};
+
+exports.down = function(knex, Promise) {
+    return knex.schema.table('users', function (table) {
+        table.dropColumn('account');
+    })
+};
