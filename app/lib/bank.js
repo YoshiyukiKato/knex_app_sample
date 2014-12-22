052da46b3eae92b80a8c5664f3c92020576d11ef
YoshiyukiKato <yo.shuuuuki@gmail.com>
2014-12-22 23:35:02 +0900

    improve indentation

--- app/lib/bank.js
+++ app/lib/bank.js
@@ -1,28 +1,28 @@
 
 function operation(knex, config){
-	return knex.transaction(function(trx) {
-		//引き出し元を探してくる
-   		return trx.select("*").from("users").where({id:config.operator})
-    	.then(function(rows){
-       		//引き出し。口座がないときはエラー
-        	if(!rows[0]) throw new Error("Abort::No such account.");
-        	
-			var sum = rows[0].account - config.withdraw;
-        	if(sum > 0) return trx.update("account",sum).from("users").where({id:rows[0].id});
-        	else throw new Error("Abort::Not enough money!");       		
-		})
-		.then(function(rows){
-			//振り込み先を探してくる
-			return trx.select("*").from("users").where({id:config.direction})
-		})
-		.then(function(rows){
-       		//振り込み。口座がないときはエラー
-       		if(!rows[0]) throw new Error("Abort::No such account.");        
+    return knex.transaction(function(trx) {
+        //引き出し元を探してくる
+        return trx.select("*").from("users").where({id:config.operator})
+        .then(function(rows){
+            //引き出し。口座がないときはエラー
+            if(!rows[0]) throw new Error("Abort::No such account.");
 
-       	 	var sum = rows[0].account + config.deposit;
-    	    return trx.update("account",sum).from("users").where({id:rows[0].id});
-    	});
-	});
+            var sum = rows[0].account - config.withdraw;
+            if(sum > 0) return trx.update("account",sum).from("users").where({id:rows[0].id});
+            else throw new Error("Abort::Not enough money!");       		
+        })
+        .then(function(rows){
+            //振り込み先を探してくる
+            return trx.select("*").from("users").where({id:config.direction})
+        })
+        .then(function(rows){
+            //振り込み。口座がないときはエラー
+            if(!rows[0]) throw new Error("Abort::No such account.");        
+
+            var sum = rows[0].account + config.deposit;
+            return trx.update("account",sum).from("users").where({id:rows[0].id});
+        });
+    });
 }
 
 exports.operation = operation;
