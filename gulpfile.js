41aa37fde14bf1064a6d254ffabbb51f95573c35
YoshiyukiKato <yo.shuuuuki@gmail.com>
2014-12-24 00:56:01 +0900

    retry travis build

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,27 @@
 var gulp = require('gulp');
 
 var knex = require("knex");
-var travis_knex = {
-    client: 'postgresql',
-    connection: {
-        database: "travis_ci_test",
-        username: "postgres"
-    },
-    pool: {
-        min: 2,
-        max: 10
-    },
-    migrations: {
-        directory: "./db/migrations",
-        tableName: 'knex_migrations'
-    },
-    seeds: {
-        directory: './db/seeds/dev'
+
+gulp.task('travis', function () {
+    var travis_knex = {
+        client: 'postgresql',
+        connection: {
+            database: "travis_ci_test",
+            username: "postgres"
+        },
+        pool: {
+            min: 2,
+            max: 10
+        },
+        migrations: {
+            tableName: 'knex_migrations'
+        },
+        seeds: {
+            directory: './db/seeds/dev'
+        }
     }
-}
-
-gulp.task('travis_migrate', function () {
-    return knex(travis_knex).migrate.latest(travis_knex);
+    knex(travis_knex).migrate.latest();
+    knex(travis_knex).seed.run();
 });
 
-gulp.task('travis_seed', function () {
-    return knex(travis_knex).seed.run(travis_knex);
-});
+
