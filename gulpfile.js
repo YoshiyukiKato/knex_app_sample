d9b41d27c74dbfb39467a87c96841ec19ee81495
YoshiyukiKato <yo.shuuuuki@gmail.com>
2014-12-24 00:58:00 +0900

    fix gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -14,14 +14,15 @@ gulp.task('travis', function () {
             max: 10
         },
         migrations: {
+            directory: "./db/migrations",
             tableName: 'knex_migrations'
         },
         seeds: {
             directory: './db/seeds/dev'
         }
     }
-    knex(travis_knex).migrate.latest();
-    knex(travis_knex).seed.run();
+    knex(travis_knex).migrate.latest(travis_knex);
+    knex(travis_knex).seed.run(travis_knex);
 });
 
 
