9cdfc83920445f46a23e5ea8fd452d5a4c6fd83d
YoshiyukiKato <yo.shuuuuki@gmail.com>
2014-12-24 01:25:43 +0900

    retry build

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,7 @@
 var gulp = require('gulp');
-
 var knex = require("knex");
 
-gulp.task('travis', function () {
+gulp.task('travis_migrate', function () {
     var travis_knex = {
         client: 'postgresql',
         connection: {
@@ -21,8 +20,72 @@ gulp.task('travis', function () {
             directory: './db/seeds/dev'
         }
     }
-    knex(travis_knex).migrate.latest(travis_knex);
-    knex(travis_knex).seed.run(travis_knex);
+    knex(travis_knex).migrate.latest();
+    return;
 });
 
+gulp.task('travis_seed', function () {
+    var travis_knex = {
+        client: 'postgresql',
+        connection: {
+            database: "travis_ci_test",
+            username: "postgres"
+        },
+        pool: {
+            min: 2,
+            max: 10
+        },
+        migrations: {
+            directory: "./db/migrations",
+            tableName: 'knex_migrations'
+        },
+        seeds: {
+            directory: './db/seeds/dev'
+        }
+    }
+    knex(travis_knex).seed.run();
+    return;
+});
+
+gulp.task('migrate', function () {
+    var travis_knex = {
+        client: 'postgresql',
+        connection: {
+            host:'localhost',
+            user: 'yoshiyuki',
+            database :'yoshiyuki'
+        },
+        migrations: {
+            directory: "./db/migrations",
+            tableName: 'knex_migrations'
+        },
+        seeds: {
+            directory: './db/seeds/dev'
+        }
+    }
+    knex(travis_knex).migrate.latest()
+    .then(function(){
+        return knex(travis_knex).seed.run();
+    });
+});
+
+gulp.task('seed', function () {
+    var travis_knex = {
+        client: 'postgresql',
+        connection: {
+            host:'localhost',
+            user: 'yoshiyuki',
+            database :'yoshiyuki'
+        },
+        migrations: {
+            directory: "./db/migrations",
+            tableName: 'knex_migrations'
+        },
+        seeds: {
+            directory: './db/seeds/dev'
+        }
+    }
+    
+    knex(travis_knex).seed.run();
+});
 
--- knexfile.js
+++ knexfile.js
@@ -10,10 +10,11 @@ module.exports = {
             database :'yoshiyuki'
         },
         migrations: {
+            directory:"./db/migrations",
             tableName: 'knex_migrations'
         },
         seeds: {
-            directory: './seeds/dev'
+            directory: './db/seeds/dev'
         }
     },
 
@@ -28,10 +29,11 @@ module.exports = {
             max: 10
         },
         migrations: {
+            directory:"./db/migrations",
             tableName: 'knex_migrations'
         },
         seeds: {
-            directory: './seeds/dev'
+            directory: './db/seeds/dev'
         }
     },
 
