a94d6e403d19390259d9be8f321ae3a7890f77e0
YoshiyukiKato <yo.shuuuuki@gmail.com>
2014-12-24 12:07:03 +0900

    retry build

--- spec/travisSpec.js
+++ spec/travisSpec.js
@@ -14,27 +14,6 @@ var knex = require("knex")({
     }
 });
 
-
-//===============BANK TEST==================
-var bank = require("../app/lib/bank.js");
-var config = {
-    operation:"deposit",
-	operator:1,
-	direction:1,
-	withdraw:0,
-	deposit:100
-}
-
-describe("Bank operation",function(){
-    it("should be return updated account data",function(done){
-        bank.operation(knex,config)
-        .then(function(message){
-            console.log(message);
-            done();
-        });
-    });
-});
-
 //=============== INTERFACE TEST==================
 var interface = require("../app/lib/interface.js");
     
@@ -73,6 +52,26 @@ describe("Banking sequence", function(){
         testEmitter("2", 500)
         .then(function(){
             return testEmitter(100, 500);
+        });
+    });
+});
+
+//===============BANK TEST==================
+var bank = require("../app/lib/bank.js");
+var config = {
+    operation:"deposit",
+	operator:1,
+	direction:1,
+	withdraw:0,
+	deposit:100
+}
+
+describe("Bank operation",function(){
+    it("should be return updated account data",function(done){
+        bank.operation(knex,config)
+        .then(function(message){
+            console.log(message);
+            done();
         });
     });
 });
